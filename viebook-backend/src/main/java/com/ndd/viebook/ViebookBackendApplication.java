d2572d6b1e229670292c21260d0ea3345c0f1934
duongngo2005 <info.duongngo@gmail.com>
2026-06-23 14:45:06 +0700

    initial project

---
+++ viebook-backend/src/main/java/com/ndd/viebook/ViebookBackendApplication.java
@@ -0,0 +1,13 @@
+package com.ndd.viebook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ViebookBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ViebookBackendApplication.class, args);
+	}
+
+}
