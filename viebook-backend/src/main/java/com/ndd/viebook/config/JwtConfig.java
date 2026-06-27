11894c65f1dc84ba367da6713908898cbff1d7e6
duongngo2005 <info.duongngo@gmail.com>
2026-06-27 14:07:57 +0700

    chore: add JWT config and required dependencies

---
+++ viebook-backend/src/main/java/com/ndd/viebook/config/JwtConfig.java
@@ -0,0 +1,15 @@
+package com.ndd.viebook.config;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ConfigurationProperties(prefix = "app.jwt")
+@Getter @Setter
+public class JwtConfig {
+    private String secret;
+    private long accessExpirationMs;
+    private long refreshExpirationMs;
+}
